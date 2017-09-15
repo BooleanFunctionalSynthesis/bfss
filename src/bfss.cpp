2da6cb413ad693931271dc86a4ec324ef7e1810e
Shubham Goel <sgoel160@gmail.com>
2017-09-15 12:55:29 +0530

    Composing SAig to obtain R-sets

--- src/bfss.cpp
+++ src/bfss.cpp
@@ -29,14 +29,36 @@ Abc_Frame_t* pAbc;
 ////////////////////////////////////////////////////////////////////////
 ///                        DECLARATIONS                              ///
 ////////////////////////////////////////////////////////////////////////
+void populateVars(Abc_Ntk_t* FNtk, AigToNNF& nnf, string varsFile,
+                    vector<int>& varsXF, vector<int>& varsXS,
+                    vector<int>& varsYF, vector<int>& varsYS,
+                    map<string,int>& name2IdF, map<int,string>& id2NameF);
+Aig_Obj_t* Aig_SubstituteConst(Aig_Man_t* pMan, Aig_Obj_t* initAig, int varId, int one);
+Aig_Obj_t* Aig_Substitute(Aig_Man_t* pMan, Aig_Obj_t* initAig, int varId, Aig_Obj_t* func);
+void initializeRs(Aig_Man_t* SAig,vector<vector<Aig_Obj_t*> >& r0, vector<vector<Aig_Obj_t*> >& r1);
 
 
 ////////////////////////////////////////////////////////////////////////
 ///                         FUNCTIONS                                ///
 ////////////////////////////////////////////////////////////////////////
 
+/** Function
+ * Reads the varsFile and populates essential maps and vectors using FNtk, nnf.
+ * @param FNtk      [in]
+ * @param nnf       [in]
+ * @param varsFile  [in]
+ * @param varsXF    [out]   maps x_i  to Id in FAig
+ * @param varsXS    [out]   maps x_i  to Id in SAig
+ * @param varsYF    [out]   maps y_i  to Id in FAig
+ * @param varsYS    [out]   maps y_i  to Id in SAig
+ * @param name2IdF  [out]   maps name to Id in FAig
+ * @param Id2nameF  [out]   maps Id in FAig to name
+ */
 void populateVars(Abc_Ntk_t* FNtk, AigToNNF& nnf, string varsFile,
-                    map<string,int>& name2IdF, map<int,string>& id2NameF) {
+    vector<int>& varsXF, vector<int>& varsXS,
+    vector<int>& varsYF, vector<int>& varsYS,
+    map<string,int>& name2IdF, map<int,string>& id2NameF) {
+
     int i;
     Abc_Obj_t* pPi;
     string line;
@@ -66,6 +88,97 @@ void populateVars(Abc_Ntk_t* FNtk, AigToNNF& nnf, string varsFile,
         varsXS.push_back(nnf.var_num2Id[it]);
     for(auto it : varsYF)
         varsYS.push_back(nnf.var_num2Id[it]);
+}
+
+/** Function
+ * Composes input variable in initiAig with @param one, returns resulting Aig_Obj
+ * @param pMan      [in out]    Aig Manager
+ * @param initAig   [in]        Specifies the head of function tree
+ * @param varId     [in]        (>=1) Specifies the variable to be substituted
+ * @param one       [in]       set to 1 if varId is to be substituted by 1
+ */
+Aig_Obj_t* Aig_SubstituteConst(Aig_Man_t* pMan, Aig_Obj_t* initAig, int varId, int one) {
+    Aig_Obj_t* const1 = Aig_ManConst1(pMan);
+    Aig_Obj_t* constf = (one? const1: Aig_Not(const1));
+    Aig_Obj_t* currFI = Aig_ObjIsCo(initAig)? initAig->pFanin0: initAig;
+    Aig_Obj_t* afterCompose = Aig_Compose(pMan, currFI, constf, varId-1);
+    return afterCompose;
+}
+
+/** Function
+ * Composes input variable in initiAig with @param func, returns resulting Aig_Obj
+ * @param pMan      [in out]    Aig Manager
+ * @param initAig   [in]        Specifies the head of function tree
+ * @param varId     [in]        (>=1) Specifies the variable to be substituted
+ * @param func      [in]        Specifies the function that supplants the input
+ */
+Aig_Obj_t* Aig_Substitute(Aig_Man_t* pMan, Aig_Obj_t* initAig, int varId, Aig_Obj_t* func) {
+    Aig_Obj_t* currFI = Aig_ObjIsCo(initAig)? initAig->pFanin0: initAig;
+    Aig_Obj_t* afterCompose = Aig_Compose(pMan, currFI, func, varId-1);
+    return afterCompose;
+}
+
+/** Function
+ * Composes inputs of SAig with appropriate delta and gamma, makes the resulting
+ * objects COs and stores them in r0 and r1.
+ * @param SAig [in out]     Aig Manager
+ * @param r0   [out]        Underapproximates the Cannot-be-0 sets
+ * @param r1   [out]        Underapproximates the Cannot-be-1 sets
+ */
+void initializeRs(Aig_Man_t* SAig,
+    vector<vector<Aig_Obj_t*> >& r0, vector<vector<Aig_Obj_t*> >& r1) {
+    for(int i = 0; i < numY; ++i) {
+        Aig_Obj_t* delta = Aig_ManCo(SAig, 0);
+        for(int j = 0; j < numX; j++) {
+            delta = Aig_Substitute(SAig, delta, varsXS[j], Aig_Not(Aig_ManCi(SAig, varsXS[j] - 1)));
+        }
+        for(int j = 0; j < numY; j++) {
+            if(j < i)
+                delta = Aig_SubstituteConst(SAig, delta, varsYS[j], 0);
+            else if(j == i)
+                delta = Aig_SubstituteConst(SAig, delta, varsYS[j], 1);
+            else
+                delta = Aig_Substitute(SAig, delta, varsYS[j], Aig_Not(Aig_ManCi(SAig, varsYS[j] - 1)));
+        }
+        for(int j = 0; j < numX; j++) {
+            delta = Aig_Substitute(SAig, delta, numOrigInputs + varsXS[j], Aig_ManCi(SAig, varsXS[j] - 1));
+        }
+        for(int j = 0; j < numY; j++) {
+            if(j <= i)
+                delta = Aig_SubstituteConst(SAig, delta, numOrigInputs + varsYS[j], 0);
+            else
+                delta = Aig_Substitute(SAig, delta, numOrigInputs + varsYS[j], Aig_ManCi(SAig, varsYS[j] - 1));
+        }
+        // for(int j = numX + numY; j < 2*(numX + numY); j++)
+            // Aig_ObjDelete(delta, Aig_ManCi(delta, j));
+        r0[i].push_back(Aig_ObjCreateCo(SAig, delta));
+
+        // reuse delta of the first half of the above for gamma
+        Aig_Obj_t* gamma = Aig_ManCo(SAig, 0);
+        for(int j = 0; j < numX; j++) {
+            gamma = Aig_Substitute(SAig, gamma, varsXS[j], Aig_Not(Aig_ManCi(SAig, varsXS[j] - 1)));
+        }
+        for(int j = 0; j < numY; j++) {
+            if(j <= i)
+                gamma = Aig_SubstituteConst(SAig, gamma, varsYS[j], 0);
+            else
+                gamma = Aig_Substitute(SAig, gamma, varsYS[j], Aig_Not(Aig_ManCi(SAig, varsYS[j] - 1)));
+        }
+        for(int j = 0; j < numX; j++) {
+            gamma = Aig_Substitute(SAig, gamma, numOrigInputs + varsXS[j], Aig_ManCi(SAig, varsXS[j] - 1));
+        }
+        for(int j = 0; j < numY; j++) {
+            if(j < i)
+                gamma = Aig_SubstituteConst(SAig, gamma, numOrigInputs + varsYS[j], 0);
+            else if(j == i)
+                gamma = Aig_SubstituteConst(SAig, gamma, numOrigInputs + varsYS[j], 1);
+            else
+                gamma = Aig_Substitute(SAig, gamma, numOrigInputs + varsYS[j], Aig_ManCi(SAig, varsYS[j] - 1));
+        }
+        // for(int j = numX + numY; j < 2*(numX + numY); j++)
+            // Aig_ObjDelete(gamma, Aig_ManCi(gamma, j));
+        r1[i].push_back(Aig_ObjCreateCo(SAig, gamma));
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////
@@ -166,7 +279,10 @@ int main( int argc, char * argv[] )
         cout << "#####################################################" << endl;
     #endif
 
-    populateVars(FNtk, nnf, varsFile, name2IdF, id2NameF);
+    populateVars(FNtk, nnf, varsFile,
+                    varsXF, varsXS,
+                    varsYF, varsYS,
+                    name2IdF, id2NameF);
     numX = varsXF.size();
     numY = varsYF.size();
 
@@ -193,6 +309,9 @@ int main( int argc, char * argv[] )
     #endif
 
     assert(numX + numY == numOrigInputs);
+
+    vector<vector<Aig_Obj_t*> > r0(numY), r1(numY);
+    initializeRs(SAig, r0, r1);
 
     // Stop ABC
     Abc_Stop();
